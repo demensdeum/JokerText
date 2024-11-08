29bd084aa2109c3f87819c0bd20c05b1a600c52d
DEMENSDEUM-ACER\Demensdeum <demensdeum@gmail.com>
2024-11-08 21:53:47 +0400

    Impl3

--- extension/JokerTextMessageTransformer.js
+++ extension/JokerTextMessageTransformer.js
@@ -1,18 +1,18 @@
-function upperEverySecondLetter(input) {
-    let output = "";
-    for (let i = 0; i < input.length; i++) {
-        let char = input[i];
-        let code = char.charCodeAt(0);
+function capitalizeFirstLowerSecond(input) {
+    var output = "";
+    for (var i = 0; i < input.length; i++) {
+        var char = input[i];
+        var code = char.charCodeAt(0);
 
-        if (i % 2 !== 0) {
-            // Каждую вторую букву делаем заглавной
+        if (i % 2 === 0) {
+            // Каждую первую букву делаем заглавной
             if (code >= 0x61 && code <= 0x7A) {
                 char = String.fromCharCode(code - 32); // Латиница, маленькие 'a'-'z' -> заглавные
             } else if (code >= 0x430 && code <= 0x44F) {
                 char = String.fromCharCode(code - 32); // Кириллица, маленькие 'а'-'я' -> заглавные
             }
         } else {
-            // Остальные буквы делаем маленькими
+            // Каждую вторую букву делаем маленькой
             if (code >= 0x41 && code <= 0x5A) {
                 char = String.fromCharCode(code + 32); // Латиница, заглавные 'A'-'Z' -> маленькие
             } else if (code >= 0x410 && code <= 0x42F) {
@@ -26,6 +26,6 @@ function upperEverySecondLetter(input) {
 }
 
 var jokerTextMessageTransformer = function(message) {
-    message = upperEverySecondLetter(message);
+    message = capitalizeFirstLowerSecond(message);
     return message;
 };
