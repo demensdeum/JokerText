2311cf0f6fd26aba11d53f9db78d6ce07234ba83
DEMENSDEUM-ACER\Demensdeum <demensdeum@gmail.com>
2024-11-08 22:44:06 +0400

    Impl15

--- extension/JokerTextMessageTransformer.js
+++ extension/JokerTextMessageTransformer.js
@@ -5,7 +5,7 @@ function capitalizeFirstLowerSecond(input) {
     var output = "";
     for (var i = 0; i < input.length; i++) {
         var code = input.charCodeAt(i);
-        var char = "".fromCharCode(code);
+        var char = input.fromCharCode(code);
 
         // if (i % 2 === 0) {
         //     // Каждую первую букву делаем заглавной
