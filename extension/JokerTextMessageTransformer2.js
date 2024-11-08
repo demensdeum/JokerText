654b83d84863c21513c2b894b5d896ada1c3a84c
DEMENSDEUM-ACER\Demensdeum <demensdeum@gmail.com>
2024-11-09 00:33:24 +0400

    Impl69

---
+++ extension/JokerTextMessageTransformer2.js
@@ -0,0 +1,42 @@
+function capitalizeFirstLowerSecond(input) {
+    var output = "";
+    for (var i = 0; i < input.length; i++) {
+        var char = input.charAt(i);
+
+        if (i % 2 === 0) {
+            output += char.toUpperCase();
+        } else {
+            output += char.toLowerCase();
+        }
+
+    }
+    return output;
+}
+
+function makeJoke(input) {
+    // var output = input + " JOKER";
+    // return output;
+
+    var output = "";
+    
+    var jokeType = "JuMp";
+
+    if (jokeType == "cypher") {
+        for (var i = 0; i < input.length; i++) {
+            var code = input.charCodeAt(i);
+            output += code;
+            output += ".";
+        }
+    }
+
+    if (jokeType == "JuMp") {
+        output = capitalizeFirstLowerSecond(input);
+    }
+
+    return output;
+}
+
+var jokerTextMessageTransformer = function(message) {
+    var output = makeJoke(message);
+    return output;
+};
