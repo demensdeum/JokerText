6700c386a740f17907bf664562b5052f13ca3c95
DEMENSDEUM-ACER\Demensdeum <demensdeum@gmail.com>
2024-11-08 23:35:20 +0400

    Impl19

--- extension/JokerTextMessageTransformer.js
+++ extension/JokerTextMessageTransformer.js
@@ -6,27 +6,7 @@ function capitalizeFirstLowerSecond(input) {
     for (var i = 0; i < input.length; i++) {
         var code = input.charCodeAt(i);
         output += code;
-        output += " ";
-        //var char = input.fromCharCode(code);
-
-        // if (i % 2 === 0) {
-        //     // Каждую первую букву делаем заглавной
-        //     if (code >= 0x61 && code <= 0x7A) {
-        //         char = String.fromCharCode(code - 32); // Латиница, маленькие 'a'-'z' -> заглавные
-        //     } else if (code >= 0x430 && code <= 0x44F) {
-        //         char = String.fromCharCode(code - 32); // Кириллица, маленькие 'а'-'я' -> заглавные
-        //     }
-        // } else {
-        //     // Каждую вторую букву делаем маленькой
-        //     if (code >= 0x41 && code <= 0x5A) {
-        //         char = String.fromCharCode(code + 32); // Латиница, заглавные 'A'-'Z' -> маленькие
-        //     } else if (code >= 0x410 && code <= 0x42F) {
-        //         char = String.fromCharCode(code + 32); // Кириллица, заглавные 'А'-'Я' -> маленькие
-        //     }
-        // }
-
-//        output += char;
-//    }
+        output += ".";
     }
     return output;
 }
